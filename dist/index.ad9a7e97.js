23d8ad74a4ae1cebc769b819b0462f49d6201980
Mike <mikemcguiness@protonmail.com>
2023-10-11 14:47:25 -0700

    add createRoot and render

--- dist/index.ad9a7e97.js
+++ dist/index.ad9a7e97.js
@@ -1,5 +1,6 @@
 const React = {
-    createElement
+    createElement,
+    createRoot
 };
 function createElement(type, props, ...children) {
     const element = {
@@ -14,13 +15,45 @@ function createElement(type, props, ...children) {
     console.log(element);
     return element;
 }
+function createRoot(domNode) {
+    console.log("creating root with element: ", domNode);
+    return {
+        render: (element)=>render(element, domNode),
+        unmount: ()=>unmount(domNode)
+    };
+}
+function render(element, domNode) {
+    console.log("rendering element: ", element);
+    if (typeof element === "string") {
+        domNode.appendChild(document.createTextNode(element));
+        return;
+    }
+    const newDomNode = document.createElement(element.type);
+    // copy props from element to new node
+    Object.keys(element.props).filter((key)=>key !== "children").forEach((prop)=>newDomNode[prop] = element[prop]);
+    domNode.appendChild(newDomNode);
+    // recurse on children
+    element.props.children.forEach((child)=>render(child, newDomNode));
+}
+function unmount(root) {
+    root.innerHTML = "";
+}
 const test = /*#__PURE__*/ React.createElement("div", {
     __source: {
         fileName: "index.tsx",
-        lineNumber: 20,
-        columnNumber: 14
+        lineNumber: 66,
+        columnNumber: 3
     },
     __self: this
-}, "hello");
+}, "parent", /*#__PURE__*/ React.createElement("div", {
+    __source: {
+        fileName: "index.tsx",
+        lineNumber: 68,
+        columnNumber: 5
+    },
+    __self: this
+}, "child"));
+const root = createRoot(document.getElementById("root"));
+root.render(test);
 
 //# sourceMappingURL=index.ad9a7e97.js.map
--- index.tsx
+++ index.tsx
@@ -1,8 +1,24 @@
+type Props = {
+  [key: string]: any;
+  children?: ReactElement[];
+  key?: string | number | null;
+  ref?: any;
+};
+
+type ReactElement = {
+  type: string;
+  props: Props;
+  key: string | number | null;
+  ref: any;
+};
+
+
 const React = {
   createElement,
+  createRoot,
 }
 
-function createElement(type, props, ...children) {
+function createElement(type: string, props: Props, ...children: ReactElement[]): ReactElement {
   const element = {
     type,
     props: {
@@ -17,4 +33,42 @@ function createElement(type, props, ...children) {
   return element;
 }
 
-const test = <div>hello</div>
+function createRoot(domNode: HTMLElement) {
+  console.log("creating root with element: ", domNode);
+  return {
+    render: (element: ReactElement) => render(element, domNode),
+    unmount: () => unmount(domNode)
+  }
+}
+
+function render(element: ReactElement, domNode: HTMLElement) {
+  console.log("rendering element: ", element);
+
+  if (typeof element === 'string') {
+    domNode.appendChild(document.createTextNode(element));
+    return;
+  }
+
+  const newDomNode = document.createElement(element.type);
+  // copy props from element to new node
+  Object.keys(element.props).filter(key => key !== 'children').forEach(prop => newDomNode[prop] = element[prop]);
+  domNode.appendChild(newDomNode);
+
+  // recurse on children
+  element.props.children.forEach(child => render(child, newDomNode));
+}
+
+function unmount(root: HTMLElement) {
+  root.innerHTML = '';
+}
+
+const test =
+  <div>
+    parent
+    <div>
+      child
+    </div>
+  </div>
+
+const root = createRoot(document.getElementById('root'));
+root.render(test);
