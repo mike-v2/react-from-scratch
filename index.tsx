c49bd279643895b8e772d81d1f2f3da7bac7fa5b
Mike <mikemcguiness@protonmail.com>
2023-10-11 18:40:11 -0700

    implement function components

--- index.tsx
+++ index.tsx
@@ -12,13 +12,21 @@ type ReactElement = {
   ref: any;
 };
 
+const ATTRIBUTE_NAME_MAP = {
+  htmlFor: 'for',
+};
+
 
 const React = {
   createElement,
   createRoot,
 }
 
-function createElement(type: string, props: Props, ...children: ReactElement[]): ReactElement {
+function createElement(type: string | Function, props: Props, ...children: ReactElement[]): ReactElement {
+  if (typeof type === 'function') {
+    return type(props);
+  }
+
   const element = {
     type,
     props: {
@@ -29,7 +37,7 @@ function createElement(type: string, props: Props, ...children: ReactElement[]):
     ref: props.ref ?? null,
   };
 
-  console.log(element);
+  console.log('createdElement: ', element);
   return element;
 }
 
@@ -37,7 +45,6 @@ function createRoot(domNode: HTMLElement) {
   console.log("creating root with element: ", domNode);
   return {
     render: (element: ReactElement) => render(element, domNode),
-    unmount: () => unmount(domNode)
   }
 }
 
@@ -49,26 +56,69 @@ function render(element: ReactElement, domNode: HTMLElement) {
     return;
   }
 
+  if (Array.isArray(element)) {
+    element.forEach(child => render(child, domNode));
+    return;
+  }
+
   const newDomNode = document.createElement(element.type);
   // copy props from element to new node
-  Object.keys(element.props).filter(key => key !== 'children').forEach(prop => newDomNode[prop] = element[prop]);
+  Object.keys(element.props).filter(key => key !== 'children').forEach(prop => {
+    if (prop === 'className') {
+      newDomNode.classList.add(element.props[prop]);
+    } else {
+      const domName = ATTRIBUTE_NAME_MAP[prop] || prop;
+      newDomNode[domName] = element.props[prop];
+    }
+  });
   domNode.appendChild(newDomNode);
 
   // recurse on children
   element.props.children.forEach(child => render(child, newDomNode));
 }
 
-function unmount(root: HTMLElement) {
-  root.innerHTML = '';
+const App = () => {
+  return (
+    <div className='container'>
+      {blogPostData.map((post, index) => (
+        <BlogPost {...post} key={index} />
+      ))}
+    </div>
+  )
 }
 
-const test =
-  <div>
-    parent
+const BlogPost = (props) => {
+  console.log("rendering props: ", props);
+  return (
     <div>
-      child
+      <h3>{props.title}</h3>
+      <p><em>by {props.author} on {props.date}</em></p>
+      <p>{props.content}</p>
     </div>
-  </div>
+  )
+}
+
+const blogPostData = [
+  {
+    title: "Demystifying React Hooks",
+    author: "Jane Doe",
+    date: "October 7, 2023",
+    content: "React Hooks were introduced in React 16.8 to allow..."
+  },
+  {
+    title: "Understanding Async/Await in JavaScript",
+    author: "John Smith",
+    date: "October 14, 2023",
+    content: "Async/Await is a modern way to handle asynchronous operations..."
+  },
+  {
+    title: "Exploring the Virtual DOM",
+    author: "Alice Johnson",
+    date: "October 21, 2023",
+    content: "The Virtual DOM is a fundamental concept in React..."
+  }
+]
+
 
 const root = createRoot(document.getElementById('root'));
-root.render(test);
+root.render(<App />);
